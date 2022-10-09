c39833c17459bc3f5f9350905e19c4eb86beee73
M-Siqueira <marcelo.silva.siqueira@gmail.com>
2022-10-09 18:27:34 -0300

    exerc

--- Lista5/ex16/Program.cs
+++ Lista5/ex16/Program.cs
@@ -7,36 +7,62 @@
         //Transpor uma matriz significa transformar suas linhas em colunas e vice-versa.
         static void Main(string[] args)
         {
-            int t1 = 3, t2 = 4;
+            int t1 = 3, t2 = 4, conta = 0,conta2 = 0;
             int[,] mat = new int[t1, t2];
+            int[] auxT = new int[t1 * t2];
             int[,] matT = new int[t2, t1];
 
             for (int i = 0; i < t1; i++)
             {
-                for(int j = 0; j < t2; j++)
+                for (int j = 0; j < t2; j++)
                 {
                     Console.WriteLine("digite os itens da matriz: ");
                     mat[i, j] = int.Parse(Console.ReadLine());
+
+                    auxT[conta] = mat[i, j];
+                    conta++;
                 }
             }
 
-            Console.WriteLine("\n");
+
+
+            Console.WriteLine("matriz 3x4");
+
+            for (int i = 0; i < t1; i++)
+            {
+                for (int j = 0; j < t2; j++)
+                {
+                    Console.Write("[" + mat[i, j] + "] ");
+                }
+                Console.WriteLine("\n");
+            }
+
+
+
+
+            Console.WriteLine("matriz 4x3"); // em andamento
+
+            for (int j = 0; j < t1; j++)
+            {
+                for (int i = 0; i < t2; i++)
+                {
+                    matT[j, i] = auxT[conta2];
+
+                    Console.Write("["+matT[j, i]+"] "); 
+                }
+                Console.WriteLine("\n");
+                conta2++;
+            }
+
+
 
             
 
 
 
-            for (int j = 0; j < t2; j++)
-            {
-                for (int i = 0; i < t1; i++)
-                {
-
-                    matT[j, i] = 
 
 
-                }
-                Console.WriteLine("\n");
-            }
+
         }
     }
 }
---
+++ Lista5/ex17/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Xml.Linq;
+
+namespace ex17
+{
+    internal class Program
+    {   
+        //17) Desafio: Fazer um algoritmo que leia uma matriz de 10 linhas por 10 colunas
+        //e escreva o elemento minimax, ou seja,o menor elemento da linha
+        //onde se encontra o
+        //maior elemento da matriz.
+        //Escreva também a linha e a coluna onde foi encontrado.
+
+        static void Main(string[] args)
+        {
+            int t = 3,pLInha=0,pColuna=0,auxMax = 0;
+            int[,] mat = new int[t, t];
+
+            for(int i = 0; i < t; i++)
+            {
+                for(int j = 0; j < t; j++)
+                {
+                    Console.WriteLine("ler a matriz linha ["+i+"] coluna: ["+j+"] ");
+                    mat[i,j] = int.Parse(Console.ReadLine());
+
+                }
+            }
+            Console.WriteLine("matriz lida: ");
+            for (int i = 0; i < t; i++)
+            {
+                for (int j = 0; j < t; j++)
+                { 
+                    Console.Write("["+mat[i,j]+"] ");
+                }
+                Console.WriteLine("\n");
+            }
+
+
+            //minimo da linha e maximo com posicao
+            Console.WriteLine("minimo da linha: ");
+
+            for (int i = 0; i < t; i++)
+            {
+                int minimo = 100000;
+                int maximo = 0;
+                
+                for (int j = 0; j < t; j++)
+                {
+                    if(mat[i,j] < minimo)
+                    {
+                        minimo = mat[i,j];
+                    }
+                    if (mat[i, j] > maximo)
+                    {
+                        maximo = mat[i, j];
+                        auxMax = maximo;
+                        pLInha = i;
+                        pColuna = j;
+                    }
+
+                }
+
+
+                Console.WriteLine("\n");
+                Console.WriteLine("minimo linha ["+i+"]:"+minimo);
+            }
+
+            Console.WriteLine("maximno valor da matriz:");
+            Console.WriteLine("numero "+auxMax+" posicao linha ["+pLInha+"] coluna ["+pColuna+"] ");
+
+
+
+        }
+    }
+}
