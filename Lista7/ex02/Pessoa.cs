211888e8578540957852daa021cbdf0ce5edcdc3
M-Siqueira <marcelo.silva.siqueira@gmail.com>
2022-10-09 20:39:37 -0300

    exerc

---
+++ Lista7/ex02/Pessoa.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex02
+{
+    internal class Pessoa
+    {
+        private string PessoaNome;
+        private string PessoaSex;
+        private int Idade;
+        private double altura;
+        private double peso;
+
+        public Pessoa(string PessoaNome, string PessoaSex, int Idade, double altura, double peso)
+        {
+
+            this.PessoaNome = PessoaNome;
+            this.PessoaSex = PessoaSex;
+            this.Idade = Idade;
+            this.altura = altura;
+            this.peso = peso;
+        }
+
+        public void exibeDados(string PessoaNome, string PessoaSex, int Idade, double altura, double peso)
+        {
+
+
+            Console.WriteLine(PessoaNome);
+            Console.WriteLine(PessoaSex);
+            Console.WriteLine(Idade);
+            Console.WriteLine(altura);
+            Console.WriteLine(peso);
+
+
+        }
+
+        public void imc(double peso, double altura)
+        {
+
+            double valor = (peso) / (altura * altura);
+
+
+            Console.WriteLine("o IMC da pessoa em questao é: "+ valor);
+        }
+
+    }
+
+}
---
+++ Lista7/ex02/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+
+namespace ex02
+{
+    internal class Program 
+    {
+
+        //        2 - Escreva uma classe Pessoa contendo
+        //        todos os atributos de uma pessoa.
+        //        Faça métodos para apresentar os dados.
+        //        Faça a leitura pelo teclado dos atributos
+        //        e crie um construtor para fazer o instanciamento.
+
+        static void Main(string[] args)
+        {
+            string PessoaNome, PessoaSex;
+            int Idade;
+            double peso, altura;
+
+            Console.WriteLine("digite o nome da pessoa:");
+            PessoaNome = Console.ReadLine();
+
+            Console.WriteLine("sexo da pessoa:(M ou F):");
+            PessoaSex = Console.ReadLine();
+
+            Console.WriteLine("digite a idade nome da pessoa:");
+            Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite a altura da pessoa:");
+            altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o peso da pessoa:");
+            peso = double.Parse(Console.ReadLine());
+
+
+            Pessoa p = new Pessoa(PessoaNome,PessoaSex,Idade,altura,peso);
+
+            p.exibeDados(PessoaNome,PessoaSex,Idade,altura,peso);
+
+            p.imc(peso, altura);
+
+            
+
+        }
+    }
+}
