d2263e41ce2211e34459026f1e7e8f1eb9733e4e
M-Siqueira <marcelo.silva.siqueira@gmail.com>
2022-10-13 17:15:43 -0300

    exerc

---
+++ OrientacaoObjeto/ooPessoa/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ooPessoa
+{
+    internal class Pessoa
+    {
+        public Pessoa()
+        {
+
+        }
+        public string Nome { get; set; }
+        public int Idade { get; set; } 
+
+        public Pessoa(int idade, string nome)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+
+        
+
+    }
+}
---
+++ OrientacaoObjeto/ooPessoa/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Security.Cryptography;
+
+namespace ooPessoa
+{
+    internal class Program
+    {
+        static List<Pessoa> pessoas;// global 
+        static void Main(string[] args)
+        {
+            int contador = 0; // variavel apenas para mostrar a posicao no foreach.
+
+            pessoas = new List<Pessoa>();
+
+            pessoas.Add(new Pessoa(10, "ricardo"));
+            pessoas.Add(new Pessoa(20, "antenor"));
+            pessoas.Add(new Pessoa(30, "ausente"));
+
+            foreach (var pessoa in pessoas)
+            {
+                contador++;
+                Console.WriteLine(pessoa.Nome + " " + pessoa.Idade + " na posicao " + (contador));
+            }
+            naoOredenada();
+            OredenadaNome();
+            Oredenadaidade();
+
+        }
+
+        static void naoOredenada()
+        {
+            Console.WriteLine("nao ordenada: \n");
+
+            pessoas.ForEach(delegate (Pessoa pessoas)
+            {
+
+                Console.WriteLine(" " + pessoas.Idade + " " + pessoas.Nome);
+
+            });
+
+        }
+
+        static void OredenadaNome()
+        {
+            pessoas.Sort(delegate (Pessoa p1, Pessoa p2)
+                {
+
+               return p1.Nome.CompareTo(p2.Nome);
+
+                });
+
+            pessoas.ForEach(delegate (Pessoa p)
+            {
+                Console.WriteLine(p.Idade + " " + p.Nome);
+            });
+
+
+        }
+
+
+        static void Oredenadaidade()
+        {
+            pessoas.Sort(delegate (Pessoa p1, Pessoa p2)
+            {
+
+                return p1.Idade.CompareTo(p2.Idade);
+
+            });
+
+            pessoas.ForEach(delegate (Pessoa p)
+            {
+                Console.WriteLine(p.Idade + " " + p.Nome);
+            });
+
+
+        }
+
+        //FindAll(delegate)
+        //Find()
+        //foreach
+        //sort
+        //CompareTo
+        //
+
+
+
+    }
+}
+
+
+
+//insert in to "vazio" na garagem. 
+//dicionarios ........
