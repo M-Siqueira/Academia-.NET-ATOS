6109a51e0a9d2b5864173bd04ddba5b429d21987
M-Siqueira <marcelo.silva.siqueira@gmail.com>
2022-10-14 13:48:47 -0300

    exerc

---
+++ Lista7/ex07/Elevador.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex07
+{
+    internal class Elevador
+    {
+        private int _andarAtual = 0 ;//terreo
+        private int _totalAndares = 10;
+        private int _totalPessoas;
+        private int _totalMaximo = 10;
+
+        public int AndarAtual { get => _andarAtual; set => _andarAtual = value; }
+        public int TotalAndares { get => _totalAndares; set => _totalAndares = value; }
+        public int TotalPessoas { get => _totalPessoas; set => _totalPessoas = value; }
+        public int TotalMaximo { get => _totalMaximo; set => _totalMaximo = value; }
+
+        public void inicializa()
+        {
+            this._andarAtual = _andarAtual;
+        }
+
+        public void entra(int entrada)
+        {
+            if((_totalPessoas + entrada) > _totalMaximo)
+            {
+                Console.WriteLine("elevador cheio.");
+            }
+            else
+            {
+                Console.WriteLine("pessoas adicionadas com exito");
+                _totalPessoas = _totalPessoas + entrada;
+            }
+        }
+        public void sai(int saida)
+        {
+            if (_totalPessoas >= 1) {
+
+                _totalPessoas = _totalPessoas - saida;
+                Console.WriteLine("elevador esvaziado com exito");
+            }
+            else
+            {
+                //nao para para sair ninguem.
+                Console.WriteLine("elevador ja esta vazio, ninguem para sair.");
+            }
+            
+        }
+        public void sobe(int andarMais)
+        {
+            if (_andarAtual == 0 )
+            {
+                _andarAtual = _andarAtual + andarMais;
+                Console.WriteLine("andar atual: " + _andarAtual);
+            }
+            
+        }
+
+        public void desce(int andarMenos)
+        {
+            if (_andarAtual == _totalAndares)
+            {
+                //desce
+                _andarAtual = _andarAtual - andarMenos;
+                Console.WriteLine("andar atual: "+_andarAtual);
+            }
+        }
+    }
+}
---
+++ Lista7/ex07/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.Intrinsics.X86;
+using System;
+
+namespace ex07
+{
+    internal class Program
+    {
+        //        7 - Crie uma classe denominada Elevador para armazenar as informações de um elevador dentro de um prédio.
+        //        A classe deve armazenar o andar atual (térreo = 0),
+        //        total de andares no prédio(desconsiderando o térreo),
+        //        capacidade do elevador e quantas pessoas estão presentes nele.
+        //        A classe deve também disponibilizar os seguintes métodos:
+        //        Inicializa : que deve receber como parâmetros a capacidade do elevador e o total de andares no prédio(os elevadores sempre começam no térreo e vazio);
+        //        Entra : para acrescentar uma pessoa no elevador(só deve acrescentar se ainda houver espaço);
+        //        Sai : para remover uma pessoa do elevador(só deve remover se houver alguém dentro dele);
+        //        Sobe : para subir um andar(não deve subir se já estiver no último andar);
+        //        Desce : para descer um andar(não deve descer se já estiver no térreo);
+        //        Encapsular todos os atributos da classe(criar os métodos set e get).
+        static void Main(string[] args)
+        {
+            int option,passageiros;
+            
+            Elevador E = new Elevador();
+
+            E.inicializa();
+
+
+            while (true)
+            {
+                Console.WriteLine("elevador:\n");
+                Console.WriteLine("1- sobe.");
+                Console.WriteLine("2- desce.");
+                option = int.Parse(Console.ReadLine());
+                Console.Beep();
+                Console.WriteLine("\n");
+
+                if (option == 1)//sobir
+                {
+
+                    Console.WriteLine("quantos passageiros?");
+                    passageiros = int.Parse(Console.ReadLine());
+
+                    E.sobe(passageiros);
+                    
+
+                }else if(option == 2)//descer
+                {
+                    Console.WriteLine("quantos passageiros?");
+                    passageiros = int.Parse(Console.ReadLine());
+                    E.desce(passageiros);
+                    
+
+                }
+
+
+
+
+            }
+        }
+    }
+}
